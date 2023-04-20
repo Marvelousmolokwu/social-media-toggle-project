fde41fa101b33b9484833287f056d79b0639927a
Marvelousmolokwu <qmolokwu@gmail.com>
2023-04-20 09:46:18 +0100

    i adjusted the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ const Header = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <header className="font-inter pt-10 pb-16 bg-VeryPaleBlue rounded-b-3xl w-full">
-      <div className="me-auto ms-auto max-w-[90%] md:max-w-[50%] lg:flex lg:flex-row lg:justify-between  lg:max-w-[78%]">
+      <div className="me-auto ms-auto max-w-[90%] md:max-w-[50%] lg:flex lg:flex-row lg:justify-between  lg:min-w-[80%] lg:ml-24 xl:min-w-[84%] ">
         <div className="border-b-2 py-6 lg:border-none">
-          <h1 className="font-bold text-3xl">Social Media Dashboard</h1>
+          <h1 className="font-bold text-3xl ">Social Media Dashboard</h1>
           <p className="font-semibold text-slate-500">
             Total Followers: 23,004
           </p>
         </div>
-        <div className="flex flex-row  justify-between p-4 lg:items-center lg:gap-3">
+        <div className="flex flex-row  justify-between p-4 lg:items-center lg:gap-3 ">
           <h4 className="font-semibold">Dark Mode</h4>
           <div
             onClick={() => setToggle(!toggle)}
